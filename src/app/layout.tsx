42206b4941266118bca6fe61ab4391e7f8583c27
navyfish216 <navyfish@kbd.biglobe.ne.jp>
2026-06-23 19:43:28 +0900

    レイアウトをコメントアウト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <header>これはヘッダーです</header>
+        {/* <header>これはヘッダーです</header> */}
         {children}
-        <footer>これはフッターです</footer>
+        {/* <footer>これはフッターです</footer> */}
       </body>
     </html>
   );
